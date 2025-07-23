3d8af7735a467ce671097f3c8370472a8a4ae090
Raashidha25 <raashidhabegumn@gmail.com>
2025-07-23 17:07:02 +0530

    Assign1 Complexity reduced and duplication removed

--- monitor.py
+++ monitor.py
@@ -2,36 +2,25 @@
 from time import sleep
 import sys
 
-
+def blinkoutput():
+  for i in range(6):
+      print('\r* ', end='')
+      sys.stdout.flush()
+      sleep(1)
+      print('\r *', end='')
+      sys.stdout.flush()
+      sleep(1)
 def vitals_ok(temperature, pulseRate, spo2):
-  if temperature > 102 or temperature < 95:
+  if ((95 <= temperature <= 102 )and (60 <= pulseRate <= 100 )and spo2 >= 90):
+    print('Vitals are OK!')
+    blinkoutput()
+    return True
+  if (95 > temperature > 102 ) :
     print('Temperature critical!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif pulseRate < 60 or pulseRate > 100:
+  if 60 > pulseRate > 100:
     print('Pulse Rate is out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  elif spo2 < 90:
+  if spo2 < 90:
     print('Oxygen Saturation out of range!')
-    for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-    return False
-  return True
+  blinkoutput()
+  return False
+  
