6ab8ee38f66df0b7e5b8d62a1b03e13929287d24
Raashidha25 <raashidhabegumn@gmail.com>
2025-09-09 12:10:13 +0530

    Added more Vitals &  language

--- monitor.py
+++ monitor.py
@@ -3,32 +3,54 @@ from time import sleep
 import sys
 
 
+from time import sleep
+import sys
 
+# Ask user for language input
+LANGUAGE = input("Choose language (EN for English, DE for German): ").strip().upper()
+if LANGUAGE not in ["EN", "DE"]:
+    print("Invalid language selected. Defaulting to English.")
+    LANGUAGE = "EN"
+
+# Message translations
+MESSAGES = {
+    "Vitals OK": {
+        "EN": "Vitals are OK!",
+        "DE": "Vitalwerte sind in Ordnung!"
+    },
+    "Out of Range": {
+        "EN": "{} is out of range!",
+        "DE": "{} liegt außerhalb des Bereichs!"
+    }
+}
 
 def blinkoutput():
-  for i in range(6):
-      print('\r* ', end='')
-      sys.stdout.flush()
-      sleep(1)
-      print('\r *', end='')
-      sys.stdout.flush()
-      sleep(1)
-      
-def vitals_ok(temperature, pulseRate, spo2):
-  
-  vitals = {
-    "Temperature": (temperature, 95, 102),
-    "Pulse Rate": (pulseRate, 60, 100),
-    "SpO2": (spo2, 90, 100)
-   }
+    for i in range(6):
+        print('\r* ', end='')
+        sys.stdout.flush()
+        sleep(1)
+        print('\r *', end='')
+        sys.stdout.flush()
+        sleep(1)
 
-  for name,(value, min, max) in vitals.items():
-    if not(min <= value <=max):
-       print(f"{name} is out of range!")
-       blinkoutput()
-       return False
-  
-  print('Vitals are OK!')
-  blinkoutput()
-  return True
-  
+def vitals_ok(temperature, pulseRate, spo2, bloodsugar, bloodpressure, respiratoryrate):
+    RetOk = True
+    vitals = {
+        "Temperature": (temperature, 95, 102),
+        "Pulse Rate": (pulseRate, 60, 100),
+        "SpO2": (spo2, 90, 100),
+        "Blood Sugar": (bloodsugar, 70, 110),
+        "Blood Pressure": (bloodpressure, 90, 150),
+        "Respiratory rate": (respiratoryrate, 12, 20)
+    }
+
+    for name, (value, min, max) in vitals.items():
+        if not (min <= value <= max):
+            print("\n" + MESSAGES["Out of Range"][LANGUAGE].format(name))
+            RetOk = False
+
+    if RetOk:
+        print(MESSAGES["Vitals OK"][LANGUAGE])
+
+    blinkoutput()
+    return RetOk
--- monitor.test.py
+++ monitor.test.py
@@ -4,10 +4,10 @@ from monitor import vitals_ok
 
 class MonitorTest(unittest.TestCase):
     def test_not_ok_when_any_vital_out_of_range(self):
-        self.assertTrue(vitals_ok(98.1, 70, 98))
-        self.assertFalse(vitals_ok(99, 102, 70))
+        self.assertTrue(vitals_ok(98.1, 70, 98,80,100,15))
+        self.assertFalse(vitals_ok(103, 102, 70,80,100,15))
+        self.assertFalse(vitals_ok(98,80,70,60,50,17))
         
-
 
 if __name__ == '__main__':
   unittest.main()
